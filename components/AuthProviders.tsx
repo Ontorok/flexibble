ae87207c72674ac7fb43596159e1a07344841aad
Nasir Ahmed <nasir.ahmed@bizzntek.com>
2023-07-20 12:36:01 +0600

    feat: update provider text to provider icon

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -2,6 +2,9 @@
 
 import { getProviders, signIn } from "next-auth/react";
 import { useState, useEffect } from "react";
+import GoogleIcon from "@/assets/icons/google.png";
+import GithubIcon from "@/assets/icons/github.png";
+import Image from "next/image";
 
 type Provider = {
   id: string;
@@ -14,8 +17,22 @@ type Provider = {
 
 type Providers = Record<string, Provider>;
 
+type ProviderType = "";
+
 const AuthProviders = () => {
   const [providers, setProviders] = useState<Providers | null>(null);
+
+  const getProviderIcon = (providerName: string) => {
+    switch (providerName) {
+      case "google":
+        return <Image src={GoogleIcon} width={40} height={40} alt={providerName} />;
+      case "github":
+        return <Image src={GithubIcon} width={40} height={40} alt={providerName} />;
+
+      default:
+        return providerName;
+    }
+  };
 
   useEffect(() => {
     const fetchProviders = async () => {
@@ -30,11 +47,9 @@ const AuthProviders = () => {
     return (
       <div>
         {Object.values(providers).map((provider, i) => (
-          <div className="flex f">
-            <button key={i} onClick={() => signIn(provider.id)}>
-              {provider.id}
-            </button>
-          </div>
+          <button key={i} onClick={() => signIn(provider.id)} className="m-2 mr-0">
+            {getProviderIcon(provider.id)}
+          </button>
         ))}
       </div>
     );
